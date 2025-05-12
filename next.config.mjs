3c59b498300ab7261595029151d77ba35842e40d
Apoorva Pant <apoorvavijaypant13@gmail.com>
2025-05-12 18:02:08 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: 'https://github.com/Apoorva-Pant/weather-app',  // Replace with your repo name
+  assetPrefix: '/weather-app/', 
+};
 
 export default nextConfig;
