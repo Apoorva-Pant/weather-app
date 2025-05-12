9b0d84465915ecddece45cb6044015b84aecc986
Apoorva Pant <apoorvavijaypant13@gmail.com>
2025-05-12 23:58:46 +0530

    Setup deploy for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  basePath: "",
-};
+const nextConfig = {};
 
 export default nextConfig;
